fe14db4f9d1caf15fabc435a8c7a179a991d5dad
thovagaushop <minhmeomeomun@gmail.com>
2023-04-26 16:05:10 +0700

    l2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function App() {
-  const [data, setData] = useState({
+  const [data, setData] = useState({total_user: 0, total_product: 0, percen: 0 ,doughnut: {
     labels: ["Red", "Blue"],
     datasets: [
       {
@@ -17,7 +17,7 @@ function App() {
         borderWidth: 1,
       },
     ],
-  });
+  }});
 
   useEffect(() => {
     const getDataForChart = async () => {
@@ -25,7 +25,7 @@ function App() {
         let dataResult = await getData();
         let doneData = dataResult.index + 1;
 
-        setData({
+        setData({total_user: dataResult.total_user, total_product: dataResult.total_product, percen: ((Math.round(dataResult.index + 1) * 100) / dataResult.total_user).toFixed(2), doughnut: {
           labels: ["Red", "Blue"],
           datasets: [
             {
@@ -39,7 +39,7 @@ function App() {
               borderWidth: 1,
             },
           ],
-        });
+        }});
       } catch (error) {
         console.log(error);
       }
@@ -50,8 +50,14 @@ function App() {
   return (
     <div className="container d-flex flex-column align-items-center">
       <h1>Get Data Status</h1>
+      <h3 style={{color: "blue", fontWeight: "bold"}}>
+        Total User : {data.total_user} | Total Product : {data.total_product}
+      </h3>
+      <h2 style={{color: "blue", fontWeight: "bold"}}>
+        {data.percen} %
+      </h2>
       <div style={{width: "400px", height: "400px", paddingTop: "50px", paddingLeft: "20px"}}>
-        <Doughnut data={data} />
+        <Doughnut data={data.doughnut} />
       </div>
     </div>
   );
