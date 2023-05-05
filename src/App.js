326db8b4efb24038546f05c2542575826e52f747
thovagaushop <minhmeomeomun@gmail.com>
2023-05-05 13:00:31 +0700

    sua ngay 5/5/2023

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function App() {
-  const [data, setData] = useState({total_user: 0, total_product: 0, percen: 0, product_done: 0 ,doughnut: {
+  const [data, setData] = useState({totalUser: 0, totalProduct: 0, percen: 0, productDone: 0 ,doughnut: {
     labels: ["Red", "Blue"],
     datasets: [
       {
@@ -23,14 +23,14 @@ function App() {
     const getDataForChart = async () => {
       try {
         let dataResult = await getData();
-        let doneData = dataResult.index + 1;
+        let doneData = dataResult.currentIndex + 1;
 
-        setData({total_user: dataResult.total_user, total_product: dataResult.total_product, percen: ((Math.round(dataResult.index + 1) * 100) / dataResult.total_user).toFixed(2), product_done: dataResult.number_product_done, doughnut: {
+        setData({totalUser: dataResult.totalUser, totalProduct: dataResult.totalProduct, percen: ((Math.round(dataResult.currentIndex + 1) * 100) / dataResult.totalUser).toFixed(2), productDone: dataResult.productDone, doughnut: {
           labels: ["Red", "Blue"],
           datasets: [
             {
               label: "# of Data",
-              data: [doneData, dataResult.total_user - doneData],
+              data: [doneData, dataResult.totalUser - doneData],
               backgroundColor: [
                 "rgba(255, 99, 132, 0.2)",
                 "rgba(54, 162, 235, 0.2)",
@@ -51,10 +51,10 @@ function App() {
     <div className="container d-flex flex-column align-items-center">
       <h1>Get Data Status</h1>
       <h3 style={{color: "blue", fontWeight: "bold"}}>
-        Done :  <span style={{color: "red"}}>{data.product_done.toLocaleString("en")}</span> / {data.total_product.toLocaleString("en")} Products
+        Done :  <span style={{color: "red"}}>{data.productDone.toLocaleString("en")}</span> / {data.totalProduct.toLocaleString("en")} Products
       </h3>
       <h3 style={{color: "blue", fontWeight: "bold"}}>
-         Total user: {data.total_user.toLocaleString("en")}
+         Total user: {data.totalUser.toLocaleString("en")}
       </h3>
       
       <h2 style={{color: "blue", fontWeight: "bold"}}>
